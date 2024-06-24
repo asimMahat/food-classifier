8e3381431e53f2dcde8d8a6d6e41076175858edb
Asim Mahat <mahatasim123@gmail.com>
2024-06-24 17:02:12 +0545

    few changes and code refactor

--- my_api.py
+++ my_api.py
@@ -1,18 +1,16 @@
 # import uvicorn
-from fastapi import FastAPI 
-from fastapi import UploadFile, File  
-from prediction import read_imagefile,predict,preprocess
+from fastapi import FastAPI, UploadFile, File, HTTPException  
+from prediction import read_imagefile,predict_image,preprocess_image
 
 app = FastAPI()
 
 @app.post("/api/predict")
 async def predict_api(file: UploadFile = File(...)):
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    extension = file.filename.split(".")[-1].lower() in ("jpg", "jpeg", "png")
     if not extension:
-        print("The image file must be in (.png, .jpg or .jpeg) format")
+       raise HTTPException(status_code=400, detail="Please upload image file only")
     image = read_imagefile(await file.read())
-    image = preprocess(image)
-    predictions = predict(image)
-    # print(predictions)
+    image = preprocess_image(image)
+    predictions = predict_image(image)
     return predictions
     
--- prediction.py
+++ prediction.py
@@ -1,47 +1,41 @@
 import os
-from PIL import Image 
 from io import BytesIO
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-import json
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow_hub as hub
+import json
 from pathlib import Path
+from PIL import Image 
 
-input_shape = (250,250)
+def load_CNN_model():
+    export_dir = Path('./models/resnet_model.h5')
+    new_model = keras.models.load_model(export_dir,custom_objects={'KerasLayer':hub.KerasLayer})
+    print("Model loaded")
+    return new_model
 
 def read_imagefile(file) -> Image.Image:
     image = Image.open(BytesIO(file))
     return image
 
-def preprocess(image : Image.Image):
-    image = image.resize(input_shape)
-    image = np.asarray(image)
-    image = image/255
-    print(image.dtype)
+def preprocess_image(image : Image.Image):
+    input_size = (250,250)
+    image = image.resize(input_size)
+    image = np.asarray(image) # np.asarray as we don't want copy of that image
+    image = image/255 # normalization
     return image 
 
-def load_model():
-    export_dir = Path('./models/resnet_model.h5')
-    # new_model = tf.lite.TFLiteConverter.from_keras_model(export_dir)
-    new_model = keras.models.load_model(export_dir,custom_objects={'KerasLayer':hub.KerasLayer})
-    print("Model loaded")
-    return new_model
+def prediction_class_index():
+    resnet_class_index = dict(json.load(open('class_index.json')))
+    index_to_label = {v:k for k,v in resnet_class_index.items()}
+    return index_to_label
 
-model = load_model()
-
-resnet_class_index = dict(json.load(open('class_index.json')))
-
-index_to_label = {v:k for k,v in resnet_class_index.items()}
-
-
-def predict(image :np.ndarray):
-    image = image.reshape(1,250,250,3)
-    outputs=model.predict(image)
-    print(type(outputs))
+def predict_image(image :np.ndarray):
+    image = image.reshape(1,250,250,3)   # batch size = 1 
+    model = load_CNN_model()
+    outputs=model.predict(image) # predict method given by pre-trained CNN model
     y_hat = np.argmax(outputs)
     predicted_idx =y_hat.item()
-    print (predicted_idx)
+    index_to_label = prediction_class_index()
     return index_to_label[predicted_idx]
     
